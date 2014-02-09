28b169be198b48db8258d61bf0c8871eb9ee90e3
smruti <smrutilal2@gmail.com>
2014-02-09 23:13:14 +0530

    Used util method to print the array

--- samples/src/main/java/home/algo/misc/RemoveDuplicateDigitsFromArray.java
+++ samples/src/main/java/home/algo/misc/RemoveDuplicateDigitsFromArray.java
@@ -1,26 +1,28 @@
 package home.algo.misc;
 
+import home.algo.Util;
+
 public class RemoveDuplicateDigitsFromArray {
 
 	public int[] removeDupliateEntries(int... nos) {
+		 
 		int tail = 0;
-		for (int i = 0; i < nos.length; i++) {
-			if (i < (nos.length - 1) && nos[i] != nos[i + 1]) {
+		for (int i = 0; i < nos.length - 1; i++)
+		{
+			if (nos[i] != nos[i + 1])
 				nos[++tail] = nos[i + 1];
-			}
 		}
-		for (int i = tail + 1; i < nos.length; i++) {
+
+		for (int i = tail + 1; i < nos.length; i++)
 			nos[i] = -1;
-		}
+		
 		return nos;
 	}
 
 	public static void main(String[] args) {
 		int[] array = new RemoveDuplicateDigitsFromArray()
 				.removeDupliateEntries(1, 1, 2, 3, 4, 4, 4, 5);
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
-		}
+		Util.print(array);
 	}
 
 }
